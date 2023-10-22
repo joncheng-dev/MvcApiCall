517336602052a0a6066a90cbe5ddc0f7c9ae83d0
Jonathan Cheng <jcheng461@gmail.com>
2023-10-22 12:54:23 -0700

    set up MVC app using dotnet new; Add functionality to make API calls

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MvcApiCall.Models;
+
+namespace MvcApiCall.Controllers;
+
+public class HomeController : Controller
+{
+	private readonly ILogger<HomeController> _logger;
+	private readonly string _apikey;
+
+	public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+	{
+		_logger = logger;
+		_apikey = configuration["NYT"];
+	}
+
+	public IActionResult Index()
+	{
+		var allArticles = Article.GetArticles(_apikey);
+		// Task<string> allArticles = Article.GetArticles(_apikey);
+		return View(allArticles);
+	}
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
